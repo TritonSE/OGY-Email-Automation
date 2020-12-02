6a0361907b3b09fbb559afe12e6a469728b212ef
Elias Fang <56064410+eliasfang@users.noreply.github.com>
2020-12-01 23:10:16 -0800

    Create knex jobs table (#10)
    * Create knex jobs table
    
    * Remove unnecessary comment

---
+++ app/database/migrations/20201201151227_create-jobs-table.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('jobs', tbl => {
+        tbl.increments('id').unique().nonNullable();
+        tbl.integer('class_id').unsigned().nonNullable();
+        tbl.timestamps(true, true); // creates 'created_at' and 'update_at' columns (useTimestamps = true, defaultToNow = true)
+        tbl.datetime('scheduled_time');
+        tbl.text('job_hash');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('jobs');
+};
