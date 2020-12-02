55060e040e0eadd8ec6713dddab40063335074a1
Elias Fang <eliasfang@gmail.com>
2020-12-02 06:43:36 -0800

    Revert "Change nonNullable to notNullable to avoid errors"
    This reverts commit 639c85222ea03395a735a68b83cea27a386ac764.

--- app/database/migrations/20201201151227_create-jobs-table.js
+++ app/database/migrations/20201201151227_create-jobs-table.js
@@ -1,8 +1,8 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable('jobs', tbl => {
-        tbl.increments('id').unique().notNullable();
-        tbl.integer('class_id').unsigned().notNullable();
+        tbl.increments('id').unique().nonNullable();
+        tbl.integer('class_id').unsigned().nonNullable();
         tbl.timestamps(true, true); // creates 'created_at' and 'update_at' columns (useTimestamps = true, defaultToNow = true)
         tbl.datetime('scheduled_time');
         tbl.text('job_hash');
