83121a85ef98b6d07bc8d6770b163ecd8cdb8a95
Shravan Konduru <42595673+shravan1k@users.noreply.github.com>
2020-12-03 23:55:40 -0800

    added javadoc comment

--- app/models/jobsModel.js
+++ app/models/jobsModel.js
@@ -1,5 +1,12 @@
 const db = require('../database/dbConfig.js')
 
+/**
+ * Inserts a job into the database
+ * 
+ * @param {"class_id" : integer, 
+ *         "scheduled_time" : DateTime, 
+ *         "job_hash" : string} job 
+ */
 async function insertJob(job){
     try {
         await db('jobs')
