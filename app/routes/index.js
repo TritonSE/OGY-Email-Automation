d2435fc5117a17b5614d39c23d9e33c539f418ae
Kai Wu <xiw069@ucsd.edu>
2020-12-07 12:06:03 -0800

    nicos token methods

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {title:'OG-YOGA'});
 });
+
+router.post('/login', function(req, res, next) {
+});
+
+//idk where to put this rn so I'll hold it here
+function generateToken(username) {
+    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s'});
+}
+
+function getTokenInfo(req, res, next) {
+    const authHeader = req.headers['authorization']
+    return authHeader && authHeader.split(' ')[1];
+}
 
 module.exports = router;
