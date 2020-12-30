6ed23ddb0d3b52d9573230dfda239d849fe1af40
Shravan Konduru <42595673+shravan1k@users.noreply.github.com>
2020-12-29 20:26:35 -0800

    update scheduler

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule');
 const { getJobs15Mins } = require('./parser');
 
 function startScript() {
-    schedule.scheduleJob('*/5 * * * * *', function () {
+    schedule.scheduleJob('*/15 * * * *', function () {
         getJobs15Mins()
     });
 }
