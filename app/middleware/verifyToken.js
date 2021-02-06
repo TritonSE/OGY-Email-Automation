a4a03225c34c6f826cdc94aeaeeb90bd96277246
Algorithmist_Girl <singh_deep_narain@yahoo.com>
2021-02-06 18:46:25 -0500

    added comments to verifyToken

--- app/middleware/verifyToken.js
+++ app/middleware/verifyToken.js
@@ -1,5 +1,12 @@
+/**
+ * This method verifyToken gets the token from a cookie &
+ * checks to see if token is defined to verify it.
+ * 
+ * @param {} request the request, from which we get the token from
+ * @param {*} result what we store as a result of whether the cookie was valid or not
+ * @param {*} nextOperation the next method to call if the token is not defined 
+ */
 function verifyToken(request, result, nextOperation) {
-    // const userToken = "HI!";
     // get token from cookie
     const userToken = request.cookies.userToken;
     // if undefined
