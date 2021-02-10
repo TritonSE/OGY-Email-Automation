6c2a8b103085ea17250458eda23b0545f57f3bcd
Kai Wu <xiw069@ucsd.edu>
2021-02-09 16:33:29 -0800

    basic login page

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,6 +11,11 @@ router.get('/', function(req, res, next) {
     res.render('login', {title: 'Login'});
 });
 
+/* GET dashboard page. */
+router.get('/dashboard', function(req, res, next) {
+    res.render('dashboard', {title: 'Dashboard'});
+});
+
 router.post('/login', async function(req, res, next) {
     if(req.body.secret_key !== process.env.TOKEN_SECRET){
         const error = "wrong secret key";
