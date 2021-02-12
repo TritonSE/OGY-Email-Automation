133e40052765039a75bb87f6a4ed9da5b6a0e4b4
Thomas Li <thomasligreat11@gmail.com>
2021-02-12 13:08:05 -0800

    fix: added jsdocs

--- app/auth/tokenUtil.js
+++ app/auth/tokenUtil.js
@@ -1,5 +1,12 @@
 const jwt = require('jsonwebtoken');
 
+/**
+ * validates token, if validated, return the encoded data otherwise
+ * return null
+ *
+ * @param token
+ * @returns {Promise<*>}
+ */
 async function validateToken(token){
     return jwt.verify(token, process.env.SECRETKEY, async function(err, authData) {
         if(err) {
@@ -10,6 +17,12 @@ async function validateToken(token){
     });
 }
 
+/**
+ * generates a token that expires in 3 hours from the user Data
+ *
+ * @param userDataPacket
+ * @returns {Promise<undefined|*>}
+ */
 async function generateToken(userDataPacket){
     return jwt.sign({userDataPacket}, process.env.SECRETKEY, {expiresIn:'3hr'});
 }
