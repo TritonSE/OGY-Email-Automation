e9e3cf3e307f82bc48d901162eae387916dc4b1b
Jacob Au <jacobau.inbox@gmail.com>
2021-02-11 18:13:25 -0800

    Update 20210210230058_add_name_and_instructor_columns.js

--- app/database/migrations/20210210230058_add_name_and_instructor_columns.js
+++ app/database/migrations/20210210230058_add_name_and_instructor_columns.js
@@ -1,16 +1,16 @@
 
 exports.up = function(knex) {
     knex.schema.table('jobs', function(table){
-        table.string('class_name');
-        table.string('instructor_first');
-        table.string('instructor_last');
+        tbl.string('class_name');
+        tbl.string('instructor_first_name');
+        tbl.string('instructor_last_name');
   })
 };
 
 exports.down = function(knex) {
     knex.scheme.table('jobs',function(table){
         table.dropColumn('class_name');
-        table.dropColumn('instructor_first');
-        table.dropColumn('instructor_last')
+        table.dropColumn('instructor_first_name');
+        table.dropColumn('instructor_last_name');
     })
 };
