0e5356e2386c8cd8478ba69030491bbaf937ecb9
Elias Fang <eliasfang@gmail.com>
2021-02-15 11:17:44 -0800

    Integrated sendReminders function

--- scripts/handler.js
+++ scripts/handler.js
@@ -1,19 +1,24 @@
 const schedule = require('node-schedule');
 const jobsModel = require('../app/models/jobsModel.js');
 const parser = require('parser.js');
-const mailer = require('../app/mailer_modules/sendEmail.js');
+const mailer = require('../modules/mailer.js');
 
 /**
- * Adds scheduled jobs for sending emails by dequeueing a list of classes
- * and creating a new node-scheduled job for each class. Then, using the parser script
- * to get the list of emails for each class, send the email to the receivers.
+ * Adds scheduled jobs for sending emails by checking for
+ * classes starting in 15 min every 15 min. Then, using
+ * the parser script to get the list of emails for each class,
+ * send the email to the emails.
  */
 async function scheduleEmail() {
-    schedule.scheduleJob('*/15 * * * *', function() {
+    schedule.scheduleJob('*/15 * * * *', async function() {
         const classArr = jobsModel.getByMinutesFromNow(15);
         classArr.forEach(async function(classInfo) {
             const id = classInfo.class_id;
-            parser.getEnrolledEmails(id, mailer.sendEmails);
+            parser.getEnrolledEmails(id, mailer.sendReminders);
         });
     });
 }
+
+module.exports = {
+    scheduleEmail
+};
