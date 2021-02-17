a847f249f028082190e9aaa086bc638dbdc63a9d
Thomas Li <30820907+ThomasLiARDJAVA@users.noreply.github.com>
2021-02-16 16:20:33 -0800

    fix: fixed comments

--- app/middleware/verifyToken.js
+++ app/middleware/verifyToken.js
@@ -2,14 +2,14 @@
  * This method verifyToken gets the token from a cookie &
  * checks to see if token is defined to verify it.
  * 
- * @param {} request the request, from which we get the token from
+ * @param {express.req} request obj when a route is called
  * @param {*} result what we store as a result of whether the cookie was valid or not
  * @param {*} nextOperation the next method to call if the token is not defined 
  */
 function verifyToken(request, result, nextOperation) {
     // get token from cookie
     const userToken = request.cookies.userToken;
-    // if undefined
+
     if (typeof userToken != 'undefined') {
         request.token = userToken;
         nextOperation();
