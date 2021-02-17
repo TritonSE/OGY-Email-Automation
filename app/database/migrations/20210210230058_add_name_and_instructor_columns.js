b66294a0d0b2e145007880ec4afb1a29ceae5594
Thomas Li <30820907+ThomasLiARDJAVA@users.noreply.github.com>
2021-02-16 16:24:07 -0800

    Hotfix/thomas liardjava/1040567903 refactor (#30)
    * fix: refactor scripts and routes
    
    * removed test file
    
    * add expire for cookies
    
    * fix: .env_sample + rm getAll from jobs model
    
    * fix: readme file structure fix
    
    * fix: login page style and form submit routes
    
    * fix: added jsdocs
    
    * fix: indents
    
    * addressed white spaces
    
    * feat: added ui and table etc
    
    * fix: database syntax

--- app/database/migrations/20210210230058_add_name_and_instructor_columns.js
+++ app/database/migrations/20210210230058_add_name_and_instructor_columns.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex) {
-    knex.schema.table('jobs', function(table){
+    return knex.schema.table('jobs', function(table){
         table.string('class_name');
         table.string('instructor_first_name');
         table.string('instructor_last_name');
@@ -8,7 +8,7 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-    knex.scheme.table('jobs',function(table){
+    return knex.schema.table('jobs',function(table){
         table.dropColumn('class_name');
         table.dropColumn('instructor_first_name');
         table.dropColumn('instructor_last_name');
