f4a4603bf5038fe9c00ff95225645579271a83b1
Algorithmist_Girl <singh_deep_narain@yahoo.com>
2021-03-04 10:28:42 -0500

    calendar.ejs file along with javascript files and fullcalendar

--- app/routes/userInterface.js
+++ app/routes/userInterface.js
@@ -6,7 +6,11 @@ const jobsModel = require('../models/jobsModel');
 router.get('/', async function(req, res, next) {
     // TODO: user Interface logic goes here
     const jobs = await jobsModel.getAll();
-    res.render('userInterface/index', {title:'userInterface', jobs});
+    res.render('userInterface/index', { title: 'userInterface', jobs });
+});
+
+router.get('/calendar', async function(req, res, next) {
+    res.render('userInterface/calendar');
 });
 
 module.exports = router;
