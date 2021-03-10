dca7d10ad7aed9ca2148c7bf69dfd1dcc535bd26
Elias Fang <eliasfang@gmail.com>
2021-03-09 16:36:47 -0800

    Update scheduleEmail function to match new code

--- scripts/handler.js
+++ scripts/handler.js
@@ -1,20 +1,25 @@
 const schedule = require('node-schedule');
 const jobsModel = require('../app/models/jobsModel.js');
-const parser = require('../scripts/parser.js');
+const clientsModel = require('../app/models/clientsModel.js');
 const mailer = require('../modules/mailer.js');
 
 /**
  * Adds scheduled jobs for sending emails by checking for
- * classes starting in 15 min every 15 min. Then, using
- * the parser script to get the list of emails for each class,
- * send the email to the emails.
+ * classes starting in 30 min every 15 min using chron job 
+ * and jobsModel script. Loop through each class to get clients
+ * and then loop through clients to get their emails. Finally,
+ * send the email to all clients of each class.
  */
 async function scheduleEmail() {
     schedule.scheduleJob('*/15 * * * *', async function() {
-        const classArr = await jobsModel.getByMinutesFromNow(15);
+        const classArr = await jobsModel.getByMinutesFromNow(30);
         classArr.forEach(async function(classInfo) {
-            const id = classInfo.class_id;
-            parser.getEnrolledEmails(id, mailer.sendReminders);
+            const id = classInfo.id;
+            const clients = clientsModel.getByJobId(id);
+            const emails = await Promise.all(clients.map(async function(client) {
+                return client.email;
+            }));
+            mailer.sendReminders(classInfo, emails);
         });
     });
 }
