07a90738e4e3da78d38b90d6ecedc250c9ee492f
Thomas Li <thomasligreat11@gmail.com>
2021-03-09 16:34:41 -0800

    fix: components+header

--- app/routes/userInterface.js
+++ app/routes/userInterface.js
@@ -6,6 +6,6 @@ const jobsModel = require('../models/jobsModel');
 router.get('/', async function(req, res, next) {
     // TODO: user Interface logic goes here
     const jobs = await jobsModel.getAll();
-    res.render('userInterface/headers', {title:'userInterface', jobs});
+    res.render('userInterface/index', {title:'userInterface', jobs});
 });
 module.exports = router;
