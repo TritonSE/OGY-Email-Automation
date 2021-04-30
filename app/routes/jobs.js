1f4803f4435fdc0d6adc2d938e6f65a19979f499
Thomas Li <30820907+ThomasLiARDJAVA@users.noreply.github.com>
2021-04-30 16:39:43 -0700

    feat+fix: added clients and jobs routes, fixed middleware (#47)

---
+++ app/routes/jobs.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const tokenUtil = require('../auth/tokenUtil');
+
+router.put('/:id', async function(req, res){
+
+});
+
+module.exports = router;
