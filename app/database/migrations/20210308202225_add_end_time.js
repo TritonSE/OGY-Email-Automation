70d9464a6723a2f5738633ed029cf85168a82c2c
Shravan Konduru <42595673+shravan1k@users.noreply.github.com>
2021-05-11 00:14:08 -0700

    Job Updates Fix (#52)
    * added clients notification toggle
    
    * minor fix
    
    * implemented fixes
    
    * modified comment
    
    * made backend fixes
    
    * minor fixes

--- app/database/migrations/20210308202225_add_end_time.js
+++ app/database/migrations/20210308202225_add_end_time.js
@@ -1,11 +1,11 @@
 exports.up = function(knex) {
-    knex.schema.table('jobs', function(table) {
+    return knex.schema.table('jobs', function(table) {
       table.datetime('class_end_time');
     })
   }
   
   exports.down = function(knex) {
-    knex.schema.table('jobs', function(table) {
+    return knex.schema.table('jobs', function(table) {
       table.dropColumn('class_end_time');
     })
   }
