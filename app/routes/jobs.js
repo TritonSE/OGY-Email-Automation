d501918b4af72b6453eb72067a7c3d19f6918471
Elias Fang <eliasfang@gmail.com>
2021-05-11 09:54:43 -0700

    Add jobsModel update function

--- app/routes/jobs.js
+++ app/routes/jobs.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const tokenUtil = require('../auth/tokenUtil');
+const jobsModel = require('../models/jobsModel');
 
 router.post('/:id', async function(req, res){
+    // await jobsModel.updateById(req.params.id, req.body);
     console.log(req.params.id);
     console.log(req.body);
 });
