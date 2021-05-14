ab4537e4dc0b6bef26686d5d8cd346b7dccbc3ec
Shravan Konduru <42595673+shravan1k@users.noreply.github.com>
2021-05-14 16:45:06 -0700

    minor fix

--- scripts/parser.js
+++ scripts/parser.js
@@ -43,7 +43,7 @@ async function _parseClasses(err,data){
             };
             const scheduled_time = (new Date(classInfo.scheduled_time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}));
             const class_end_time = (new Date(classInfo.class_end_time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}));
-            job.scheduled_message = "Get ready and get pumped for your upcoming " + scheduled_time + " class! It starts at " + scheduled_time + " and ends at " + class_end_time + ". Make sure to join on time and have everything you need to have a successful and worthwhile session. And don't forget to have fun!"
+            job.scheduled_message = "Get ready and get pumped for your upcoming " + job.class_name + " class! It starts at " + scheduled_time + " and ends at " + class_end_time + ". Make sure to join on time and have everything you need to have a successful and worthwhile session. And don't forget to have fun!"
             const clients = await Promise.all(classJson.Clients.map(async function(client){
                 const clientEntry = {
                     first_name : client.FirstName,
