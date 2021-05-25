b5e2505549259e0d45a8e0b1ad7adc20ca27ac9b
David Hacker <dmhacker@protonmail.com>
2021-05-24 21:30:00 -0700

    Send GET to keepalive IP every 10 minutes

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 const schedule = require('node-schedule');
 const parser = require('./parser');
+const http = require("http");
 
 /**
  * The main function that gets called when the app starts running
@@ -10,6 +11,12 @@ function startScript() {
     schedule.scheduleJob('*/15 * * * *', async function () {
         await parser.getJobsInWeek();
     });
+    // Used to keep the scheduler alive on Heroku
+    if (process.env.KEEPALIVE_SERVER_IP) {
+      setInterval(function() {
+          http.get(process.env.KEEPALIVE_SERVER_IP);
+      }, 600000);
+    }
 }
 
 module.exports = {startScript};
